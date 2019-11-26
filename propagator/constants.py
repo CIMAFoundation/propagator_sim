c5e9fcef31f0158a06b65e19b2f929708290cc82
baghino.francesco <baghino.francesco@gmail.com>
2019-11-26 16:49:24 +0100

    RoS come funzione+mess errore se sbagli (gira)

--- propagator/constants.py
+++ propagator/constants.py
@@ -24,3 +24,11 @@ n_arr = array([(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1,
 dist = array([[1.414, 1, 1.414], [1, 0, 1], [1.414, 1, 1.414]])
 angle = array([[pi*3/4, pi/2, pi/4], [pi, np.nan, 0], [-pi*3/4, -pi/2, -pi/4]])
 cellsize = 20
+
+#parametri Rothermel
+alpha1 = 0.0693
+alpha2 = 0.0576
+#parametri Wang
+beta1 = 0.1783
+beta2 = 3.533
+beta3 = 1.2
