4820688276ab3f8425505a3cf026bdc908def78c
mirkodandrea <mirko.dandrea@gmail.com>
2025-05-13 12:44:21 +0200

    add thread locking

--- propagator/scheduler.py
+++ propagator/scheduler.py
@@ -1,48 +1,59 @@
 from dataclasses import dataclass, field
 from sortedcontainers import SortedDict
 from .events import EventData
+from threading import Lock
 
-
-@dataclass(frozen=True)
+@dataclass
 class Scheduler:
     """
-    handles the scheduling of the propagation procedure
+    Scheduler class for managing the timing of events.
     """
     events: SortedDict[int, list[EventData]] = field(default_factory=SortedDict)
+    _lock: Lock = field(default_factory=Lock, init=False, repr=False)
 
     def add(self, event: EventData):
-        if event.time not in self.events:
-            events_at_time = []
-            self.events[event.time] = events_at_time
-        else:
-            events_at_time = self.events[event.time]
-        events_at_time.append(event)
+         with self._lock:
+            if event.time not in self.events:
+                events_at_time = []
+                self.events[event.time] = events_at_time
+            else:
+                events_at_time = self.events[event.time]
+            events_at_time.append(event)
 
     def pop(self) -> tuple[int, list[EventData]]:
-        time, events = self.events.popitem(index=0)
-        return time, events
-
-    # def active(self):
-    #     """
-    #     get all the threads that have a scheduled update
-    #     :return:
-    #     """
-    #     active_t = np.unique(
-    #         [e for k in self.updates.keys() for c in self.updates[k] for e in c[:, 2]]
-    #     )
-    #     return active_t
+        with self._lock:        
+            time, events = self.events.popitem(index=0)
+            return time, events
 
     def __len__(self):
-        return len(self.updates)
+        return len(self.events)
 
     def next_time(self):
         """
         get the next time step
         :return:
         """
-        if len(self) == 0:
-            return None
+        with self._lock:        
+            if len(self) == 0:
+                return None
 
-        next_time, _next_events = self.events.peekitem(index=0)
-        return next_time
+            next_time, _next_events = self.events.peekitem(index=0)
+            return next_time
+    
+    def peek(self) -> tuple[int, list[EventData]] | None:
+        with self._lock:
+            if not self.events:
+                return None
+            return self.events.peekitem(index=0)
 
+    def next_events_times(self) -> list[int]:
+        """
+        get the list of scheduld events times
+        :return: list of times
+        """
+        with self._lock:        
+            if len(self) == 0:
+                return None
+
+            times = self.events.keys()
+            return times
