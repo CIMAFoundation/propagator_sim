b307ffe6b08d774de6a578b993aa12f73aaa024e
mirkodandrea <mirko.dandrea@gmail.com>
2025-11-02 17:44:55 +0100

    feat: 3d ignition masks in boundary conditions

--- src/propagator/core/propagator.py
+++ src/propagator/core/propagator.py
@@ -296,10 +296,18 @@ class Propagator:
 
         if boundary_condition.ignition_mask is not None:
             ign_arr = boundary_condition.ignition_mask
-            points = np.argwhere(ign_arr > 0)
+            # check ignition_mask shape beforehand
 
-            points_repeated = np.repeat(points, self.realizations, axis=0)
-            realizations = np.tile(np.arange(self.realizations), len(points))
+            points = np.argwhere(ign_arr)
+
+            if len(boundary_condition.ignition_mask.shape) == 2:
+                points_repeated = np.repeat(points, self.realizations, axis=0)
+                realizations = np.tile(
+                    np.arange(self.realizations), len(points)
+                )
+            else:
+                points_repeated = points
+                realizations = points[:, 2]
 
             fireline_intensity = np.zeros_like(
                 points_repeated[:, 0], dtype=np.float32
